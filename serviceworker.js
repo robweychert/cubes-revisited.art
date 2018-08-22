3231e936dac31c116f787ec06da31bee36a77ed6
Rob Weychert <rob@robweychert.com>
2018-08-22 08:50:00 -0400

    Adds cache management to service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,62 @@
-// addEventListener('install', function(event) {
-// 	console.log('The service worker is installing...');
-// });
+const version = 'V0.01';
+const staticCacheName = version + 'staticfiles';
 
-// addEventListener('activate', function(event) {
-// 	console.log('The service worker is activated.');
-// });
+addEventListener( 'install', installEvent => {
+	skipWaiting();
+	installEvent.waitUntil(
+		caches.open(staticCacheName)
+		.then( staticCache => {
+			// Nice to have: these items won't block the installation of the Service Worker
+			// staticCache.addAll([]);
+			// Must have: these items must be cached for the Service Worker to complete installation
+			return staticCache.addAll([
+				'styles.css',
+				'sprites.svg',
+				'scripts.js'
+			]); // end return addAll
+		}) // end open then
+	); // end waitUntil
+}); // end addEventListner
 
 addEventListener('fetch', fetchEvent => {
 	const request = fetchEvent.request;
 	fetchEvent.respondWith(
-		fetch(request)
-		.then(responseFromFetch => {
-			return responseFromFetch;
-		}) // end fetch then
-		.catch(error => {
-			return new Response(
-				'<h1>Oops!</h1> <p>Something went wrong.</p>', {
-					headers: {'Content-type': 'text/html; charset=utf-8'}
-				}
-			);
-		})
-	);
-});
+		caches.match(request)
+		.then( responseFromCache => {
+			if (responseFromCache) {
+				return responseFromCache;
+			} // end if
+			// Otherwise fetch from the network
+			return fetch(request);
+		}) // end match then
+		// fetch(request)
+		// .then(responseFromFetch => {
+		// 	return responseFromFetch;
+		// }) // end fetch then
+		// .catch(error => {
+		// 	return new Response(
+		// 		'<h1>Oops!</h1> <p>Something went wrong.</p>', {
+		// 			headers: {'Content-type': 'text/html; charset=utf-8'}
+		// 		}
+		// 	);
+		// }) // end fetch catch
+	); // end respondWith
+}); // end addEventListner
+
+addEventListener('activate', activateEvent => {
+	activateEvent.waitUntil(
+		caches.keys()
+		.then( cacheNames => {
+			return Promise.all(
+				cacheNames.map( cacheName => {
+					if (cacheName != staticCacheName) {
+						return caches.delete(cacheName);
+					} // end if
+				}) // end map
+			); // end return Promise.all
+		}) // end keys then
+		.then( () => {
+			return clients.claim();
+		}) // end then
+	); // end waitUntil
+}); // end addEventListener
