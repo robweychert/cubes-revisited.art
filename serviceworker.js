d3c609e1542da63ab9f99fe546565e56f2bb0865
Rob Weychert <rob@robweychert.com>
2018-08-24 09:32:27 -0400

    Refines service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,8 +1,9 @@
-const version = 'V0.01';
+const version = 'V0.02';
 const staticCacheName = version + 'staticfiles';
 const imageCacheName = 'images';
+const pagesCacheName = 'pages';
 
-const cacheList = [ staticCacheName, imageCacheName ];
+const cacheList = [ staticCacheName, imageCacheName, pagesCacheName ];
 
 addEventListener( 'install', installEvent => {
 	skipWaiting();
@@ -16,7 +17,8 @@ addEventListener( 'install', installEvent => {
 				'/assets/styles.css',
 				'/assets/sprites.svg',
 				'/assets/scripts.js',
-				'/offline.html'
+				'/offline.html',
+				'/fallback.svg'
 			]); // end return addAll
 		}) // end open then
 	); // end waitUntil
@@ -24,18 +26,81 @@ addEventListener( 'install', installEvent => {
 
 addEventListener('fetch', fetchEvent => {
 	const request = fetchEvent.request;
+
 	// When the user requests an HTML file
 	if (request.headers.get('Accept').includes('text/html')) {
+
+		// When the requested page is an article
+		if (/\/articles\/.+/.test(request.url)) {
+			fetchEvent.respondWith (
+				// Look in the cache
+				caches.match(request)
+				.then( responseFromCache => {
+					if (responseFromCache) {
+						// Fetch a fresh version from the network
+						fetchEvent.waitUntil(
+							fetch(request)
+							.then( responseFromFetch => {
+								// Update the cache
+								caches.open(pagesCacheName)
+								.then( pagesCache => {
+									return pagesCache.put(request, responseFromFetch);
+								}); // end open then
+							}) // end fetch then
+						); // end waitUntil
+						return responseFromCache;
+					} // end if
+					// Otherwise fetch the page from the network
+					return fetch(request)
+					.then( responseFromFetch => {
+						// Put a copy in the cache
+						const copy = responseFromFetch.clone();
+						fetchEvent.waitUntil(
+							caches.open(pagesCacheName)
+							.then( pagesCache => {
+								return pagesCache.put(request, copy);
+							}) // end open then
+						); // end waitUntil
+						return responseFromFetch;
+					}) // end fetch then
+					.catch( error => {
+						// Otherwise show the fallback page
+						return caches.match('/offline.html');
+					}); // end fetch catch and return
+				}) // end match then
+			); // end respondWith
+			return; // Go no further
+		} // end if
+
 		fetchEvent.respondWith(
-		// Fetch that page from the network
+		// Otherwise fetch the page from the network
 			fetch(request)
+			.then( responseFromFetch => {
+				// Put a copy in the cache
+				const copy = responseFromFetch.clone();
+				fetchEvent.waitUntil(
+					caches.open(pagesCacheName)
+					.then( pagesCache => {
+						return pagesCache.put(request, copy);
+					}) // end open then
+				); // end waitUntil
+				return responseFromFetch;
+			}) // end fetch then
 			.catch( error => {
+				// Otherwise look for a cached version of the page
+				return caches.match(request)
+				.then( responseFromCache => {
+					if (responseFromCache) {
+						return responseFromCache;
+					} // end if
 				// Otherwise show the fallback page
 				return caches.match('/offline.html');
+				}); // end match then and return
 			}) // end fetch catch
 		); // end respondWith
 		return; // Go no further
 	} // end if
+
 	// When the user requests an image
 	if (request.headers.get('Accept').includes('image')) {
 		fetchEvent.respondWith(
@@ -43,6 +108,17 @@ addEventListener('fetch', fetchEvent => {
 			caches.match(request)
 			.then( responseFromCache => {
 				if (responseFromCache) {
+					// Fetch a fresh version from the network
+					fetchEvent.waitUntil(
+						fetch(request)
+						.then( responseFromFetch => {
+							//Update the cache
+							caches.open(imageCacheName)
+							.then( imageCache => {
+								return imageCache.put(request, responseFromFetch);
+							}); // end open then
+						}) // end fetch then
+					); // end waitUntil
 					return responseFromCache;
 				} // end if
 				// Otherwise fetch the image from the network
@@ -57,11 +133,16 @@ addEventListener('fetch', fetchEvent => {
 						}) // end open then
 					); // end waitUntil
 					return responseFromFetch;
-				}); // end fetch then and return
+				}) // end fetch then and return
+				.catch( error => {
+					// Otherwise show a fallback image
+					return caches.match('fallback.svg');
+				}); // end fetch catch and return
 			}) // end match then
 		); // end respondWith
 		return; // Go no further
 	} // end if
+
 	// For everything else...
 	fetchEvent.respondWith(
 		// Look for a cached copy of the file
@@ -74,6 +155,7 @@ addEventListener('fetch', fetchEvent => {
 			return fetch(request);
 		}) // end match then
 	); // end respondWith
+
 }); // end addEventListner
 
 addEventListener('activate', activateEvent => {
